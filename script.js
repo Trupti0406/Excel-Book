06de3b62a4f03487d85c8689cf6f3864744413f6
trupti0406 <truptivijayyadav27@gmail.com>
2022-07-14 19:46:33 +0530

    Not done yet

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
-let topRow = document.querySelector(".top_row");
-let leftCol = document.querySelector(".left_col");
+const topRow = document.querySelector(".top_row");
+const leftCol = document.querySelector(".left_col");
 let grid = document.querySelector(".grid");
-// left column ke cells
+
+// Left Colum cells 1 to 100
 for (let i = 1; i <= 100; i++) {
-  // create a cell
+  //create a cell
   let div = document.createElement("div");
   div.setAttribute("class", "cell");
   div.textContent = i;
   leftCol.appendChild(div);
 }
-// top row ke cells
+
+// Top row cell A to z
 for (let i = 1; i <= 26; i++) {
-  // create a cell
+  //create a cell
   let div = document.createElement("div");
   div.setAttribute("class", "cell");
   div.textContent = String.fromCharCode(i + 64);
   topRow.appendChild(div);
 }
-// grid
-for (let i = 1; i <= 100; i++) {
+
+// Creating grid
+for (let cols = 1; cols <= 100; cols++) {
   let row = document.createElement("div");
   row.setAttribute("class", "row");
-  for (let j = 1; j <= 26; j++) {
+  for (let rows = 1; rows <= 26; rows++) {
     let col = document.createElement("div");
     col.setAttribute("class", "cell");
-    col.textContent = `${String.fromCharCode(64 + j)} - ${i}`;
+    col.textContent = `${String.fromCharCode(64 + rows)} - ${cols}`;
     row.appendChild(col);
   }
   grid.appendChild(row);
